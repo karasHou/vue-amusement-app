b90b753e21c916e7b3184cfc2fd6d8da985a4d8e
houweix <houwei19960927@126.com>
2018-03-02 17:10:48 +0800

    修复文件问题

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import Router from "vue-router";
-import HelloWorld from "@/components/HelloWorld";
 import Movie from "@/components/movie/Movie";
 import MovieList from "@/components/movie/MovieList";
 import MovieDetail from "@/components/movie/MovieDetail";
